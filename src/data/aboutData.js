11506bfa3e7f9603c8c99fa1c562be2487d8bdda
umakandra <umakandra24@gmail.com>
2024-11-20 14:12:17 -0800

    removedspace

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Uma Maheswari ",
+    description1: "My name's Uma Maheswari",
     description2: "I have extensive experience with Python, using it for various applications including data analysis, automation, web scraping, and building APIs,I am proficient in cloud platforms, particularly AWS. I have hands-on experience with EC2, S3, Lambda, RDS, and CloudFormation, and I am skilled in designing and managing cloud-based infrastructure. I specialize in SQL database design, optimization, and query performance tuning. I have worked with MySQL, PostgreSQL, and SQL Server, ensuring reliable data management and efficient reporting. ",
     image: 1
 }
